c845bba36a9c0450fd3f14cb1aa4e808bfc323c8
Arthur Pirolo de Oliveira <2011420083@fema.edu.br>
2022-05-25 13:46:49 -0300

    Criação dos crud de funcionario e cliente

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/cliente', function () {
+        return view('./cliente/listarCliente');
+});
+
+Route::get('/funcionario', function () {
+    return view('./funcionario/listarFuncionario');
+});
